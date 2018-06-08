d27e83f78e0223fcb505913d7b2b871556bd5420
Abdulmomen Kadum عبدالمؤمن كاظم <a.bluemix@gmail.com>
2018-06-08 20:06:54 +0300

    corner notches

--- lib/wavy_clipper.dart
+++ lib/wavy_clipper.dart
@@ -2,19 +2,47 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 class NotchedClipper extends CustomClipper<Path> {
-
   @override
   Path getClip(Size size) {
+    const double radius = 15.0;
 
     Path path = new Path();
-    path.lineTo(0.0, size.height);
 
+    Rect topLeftOval =
+        new Rect.fromCircle(center: new Offset(0.0, 0.0), radius: radius);
+    path.addArc(topLeftOval, (math.pi / 180) * 0, (math.pi / 180) * 180);
 
-    path.lineTo(size.width, size.height);
-    path.lineTo(size.width, 0.0);
+    // path.lineTo(0.0, radius);
+    // path.lineTo(0.0, radius);
+    path.lineTo(0.0, size.height - radius);
 
-    Rect oval = new Rect.fromCircle(center: new Offset(size.width / 2, 0.0), radius: 15.0);
-    path.addArc(oval, (math.pi/180) * 0, (math.pi/180) * 270);
+    // Rect bottomLeftOval = new Rect.fromCircle(center: new Offset(0.0, size.height), radius: radius);
+    // path.addArc(bottomLeftOval, (math.pi/180) * 5, (math.pi/180) * 5);
+
+    path.relativeArcToPoint(new Offset(radius, radius),
+        radius: const Radius.circular(radius));
+
+    path.lineTo(size.width - radius, size.height);
+
+    path.relativeArcToPoint(new Offset(radius * 2, 0.0),
+        radius: const Radius.circular(radius));
+
+    // path.lineTo(size.width, size.height);
+
+    // path.relativeArcToPoint(new Offset(50.0, radius * 3),
+    //     radius: const Radius.circular(radius));
+
+    // path.lineTo(size.width, size.height - radius);
+    path.lineTo(size.width, size.height - radius);
+    path.lineTo(size.width, radius);
+
+    path.relativeArcToPoint(new Offset(-radius, -radius),
+        radius: const Radius.circular(radius));
+
+    // path.lineTo(size.width - (radius * 1), 0.0);
+
+    // Rect oval = new Rect.fromCircle(center: new Offset(size.width / 2, 0.0), radius: 15.0);
+    // path.addArc(oval, (math.pi/180) * 0, (math.pi/180) * 270);
 
     // path.lineTo(0.0, 0.0);
 
@@ -28,7 +56,6 @@ class NotchedClipper extends CustomClipper<Path> {
 class BottomWaveClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-
     Path path = new Path();
     path.lineTo(0.0, size.height - 20);
 
@@ -39,7 +66,6 @@ class BottomWaveClipper extends CustomClipper<Path> {
 
     path.lineTo(size.width, size.height - 30);
     path.lineTo(size.width, 20.0);
-
 
     var bottomControlPoint2 = new Offset(size.width / 3.25, 0.0);
     var bottomEndPoint2 = new Offset(0.0, 40.0);
